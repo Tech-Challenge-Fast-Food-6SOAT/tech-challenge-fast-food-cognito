c97fd532d820982b5a048513c68cc4417fbd94d6
Danilo Cassola <danilocassola@gmail.com>
2024-09-22 12:09:36 -0300

    add cognito terraform

---
+++ main/main.tf
@@ -0,0 +1,27 @@
+resource "aws_cognito_user_pool" "lanchonete_pool" {
+  name = "lanchonete_pool"
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "admin_only"
+      priority = 1
+    }
+  }
+
+  password_policy {
+    minimum_length    = 6
+    require_lowercase = false
+    require_numbers   = false
+    require_symbols   = false
+    require_uppercase = false
+  }
+}
+
+resource "aws_cognito_user_pool_client" "lanchonete_client" {
+  name = "lanchonete_client"
+
+  user_pool_id                  = aws_cognito_user_pool.lanchonete_pool.id
+  generate_secret               = false
+  refresh_token_validity        = 90
+  prevent_user_existence_errors = "ENABLED"
+}
